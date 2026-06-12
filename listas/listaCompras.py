6672768af2cb1b3c7757542541623ae0d7f6e4b2
Bruno Otávio Sampaio de Vasconcelos <brunootaviovasconcelos@gmail.com>
2026-06-12 01:46:58 -0300

    Adicionando primeiros exercícios de Listas.

---
+++ listas/listaCompras.py
@@ -0,0 +1,12 @@
+compras = []
+
+produto1 = input("Informe o primeiro produto para adicioná-lo à lista de compras.")
+compras.append(produto1)
+
+produto2 = input("Informe o segundo produto para adicioná-lo à lista de compras.")
+compras.append(produto2)
+
+produto3 = input("Informe o terceiro produto para adicioná-lo à lista de compras.")
+compras.append(produto3)
+
+print(f"Aqui está a sua lista de compras: {compras}")
---
+++ listas/listaNomes.py
@@ -0,0 +1,13 @@
+nomes = []
+
+nome1 = input("Informe o primeiro nome para adicioná-lo à lista de nomes.")
+nomes.append(nome1)
+
+nome2 = input("Informe o segundo nome para adicioná-lo à lista de nomes.")
+nomes.append(nome2)
+
+nome3 = input("Informe o terceiro nome para adicioná-lo à lista de nomes.")
+nomes.append(nome3)
+
+print(f"Lista de nomes: {nomes}")
+print(f"A quantidade de nomes é: {len(nomes)}")
